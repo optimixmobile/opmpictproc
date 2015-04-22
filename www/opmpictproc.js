1250cb1ae9bcb4714ac5ef144d2c858587c31d2c
optimixmobile <opti-mix@hotmail.fr>
2015-04-22 18:43:44 +0200

    Create opmpictproc.js

---
+++ www/opmpictproc.js
@@ -0,0 +1,19 @@
+var pictprocess = {
+	test01: function(parm1, parm2, parm3, parm4, parm5, successCallback, errorCallback) {
+		cordova.exec(
+			successCallback, 	// success callback function
+			errorCallback, 		// error callback function
+			'pictprocess', 		// mapped Java class called "pictprocess"
+			'test01',   		// with this action name
+			[
+				{ // and this array of custom arguments to create our entry
+					"parm1": parm1,
+					"parm2": parm2,
+					"parm3": parm3,
+					"parm4": parm4,
+					"parm5": parm5
+				}
+			]
+		);
+	}
+}
