50cc865c82d7d6b081d6a1a40188b1cb1504ed07
optimixmobile <opti-mix@hotmail.fr>
2015-04-23 14:03:59 +0200

    Update and rename opmpictproc.js to pictprocess.js

--- www/pictprocess.js
+++ www/pictprocess.js
@@ -1,12 +1,12 @@
 var exec = require('cordova/exec');
 
-var opmpictproc = {
+var pictprocess = {
 	test01: function(parm1, parm2, parm3, parm4, parm5, successCallback, errorCallback) {
 		alert('ok1');
 		exec(
 			successCallback, 	// success callback function
 			errorCallback, 		// error callback function
-			'opmpictproc', 		// mapped Java class called "pictprocess"
+			'pictprocess', 		// mapped Java class called "pictprocess"
 			'test01',   		// with this action name
 			[
 				{ // and this array of custom arguments to create our entry
